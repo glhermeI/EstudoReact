e644580cd56daef0f5fb79fd56638e99b131a35d
GUILHERME FRANCISCO DA SILVA <guilherme.fsilva67@senacsp.edu.br>
2022-05-13 09:45:11 -0300

    Definindo Styles dentro do JavaScript

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,14 +1,15 @@
 function App() {
-  let Valor1 = Math.round(Math.random() *200);
-  let Valor2 = Math.round(Math.random() *200);
   
-  
-  
+  let styles = {
+    backgroundColor:"#f00",
+    color: "#fff",
+    display: "flex",
+    height: "100vh",
+  }
   return (
-    <div>
-      <p>A Soma entre 50 e 10 é: {50+10}</p>
-      <p>A Soma entre {Valor1} e {Valor2} é: {Valor1 + Valor2}</p>
-      <p>O resultado acima é {(Valor1 + Valor2) %2 === 0 ? "Par" : "Impar"}</p>
+    <div style={styles}>
+      <p>Veja o background </p>
+      
     </div>
   );
 }

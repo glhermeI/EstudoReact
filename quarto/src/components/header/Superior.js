e162f818725633bf4ad11b5940459b69d8216145
GUILHERME FRANCISCO DA SILVA <guilherme.fsilva67@senacsp.edu.br>
2022-05-16 11:57:00 -0300

    Criação de pagina usando props no react

---
+++ quarto/src/components/header/Superior.js
@@ -0,0 +1,15 @@
+import Cabecalho from "./Cabecalho";
+import Logo from "./Logo";
+
+export default function Superior(){
+    const dados ={
+        img: "./img/logo1.png",
+        usuario:"Guilherme "
+    }
+    return(
+        <div className="header">
+            <Logo img= {dados.img}/>
+            <Cabecalho user ={dados.usuario}/>
+        </div>
+    )
+}

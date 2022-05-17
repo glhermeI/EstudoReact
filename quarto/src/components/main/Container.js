1a4ddfd323373aee1c9358f0aa069f1eff875183
GUILHERME FRANCISCO DA SILVA <guilherme.fsilva67@senacsp.edu.br>
2022-05-17 11:58:33 -0300

    Configurando paginas usando servidor react

--- quarto/src/components/main/Container.js
+++ quarto/src/components/main/Container.js
@@ -1,11 +1,39 @@
 import Conteudo from "./Conteudo";
 import Mensagens from "./Mensagens";
+import {useState,useEffect} from "react";
 
 export default function Container(){
+    //Vamos usar o comando useState para iniciar o estado de dados 
+    //dos produtos. Depois usaremos o comando useEffect para carregar os produtos que vem da api.
+    //O comando useEffect é um comando de laço, portanto é necessario fazer ele parar o laço quando termina de carregar os dados.
+    //Isso é feito usando um colchetes ao final das instruções. Para obter os dados iremops utilizar o comando fetch dentro do useEffect.
+
+    const [produtos,setProdutos] = useState ([
+        {
+            id:"",
+            nome:"",
+            categoria:"",
+            preco:"",
+            msg:[],
+        },
+
+    ]);
+    useEffect(()=>{
+        fetch("http://10.26.44.32:5500/")
+        .then((response)=>response.json())
+        .then((dados)=>{
+            setProdutos(dados.output);
+    })
+    },[])
+
+
+
+
+
     return(
         <div className="container">
-            <Mensagens />
-            <Conteudo />
+            <Mensagens dados={produtos.msg}/>
+            <Conteudo dados={produtos}/>
         </div>
     )
 }
--- quarto/src/components/main/Conteudo.js
+++ quarto/src/components/main/Conteudo.js
@@ -1,7 +1,23 @@
-export default function Conteudo(){
+
+export default function Conteudo(props){
+    //Vamos iniciar o projeto lendo a url com os dados da api
+    //neste caso temos os produtos para ler montar a tela de conteudo de
+    
     return(
         <div className="conteudo">
             <h2>Produtos</h2>
+            <div className="itens">
+            {
+                props.dados.map((itens,index) =>(
+                    <div key={index}>
+                      <h3>{itens.nome}</h3>
+                      <p>{itens.nome}</p>
+                      <p>{itens.categoria}</p>
+                      <p>R$ {itens.preco}</p>
+                    </div>
+                ))
+            }
+            </div>
         </div>
     )
 }
